1b2661cbd6fb0dfb108782ade7f7cd1f7b21c626
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-01-01 21:55:15 -0600

    helper script to commit changes

---
+++ commit_modified_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get a list of all modified files
+modified_files=$(git ls-files --modified)
+
+# Loop through each modified file
+for file in $modified_files
+do
+    # Add file to staging
+    git add $file
+
+    # Commit the file with a message
+    git commit -m "Modified $file"
+done
+
